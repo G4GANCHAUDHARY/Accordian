7ec1d6a8108695084208f8883a166e75ad716844
G4GANCHAUDHARY <chaudharygagan3944@gmail.com>
2022-01-17 21:52:27 +0530

    Only One Open Accordian Done

--- src/App.js
+++ src/App.js
@@ -1,14 +1,44 @@
 import './App.css';
 import data from './data/data';
-import Accordian from './components/accordian';
+// import Accordian from './components/accordian';
+import { useState,useEffect } from 'react';
 
 function App() {
+  const [active,setActive]=useState([]);
+
+  useEffect(()=>{
+    const array=[];
+    for (let i=0;i<data.length;i++){
+      array.push(false)
+    }
+    setActive(array)
+  },[])
+
+  const changeState=(index)=>{
+    const array=[];
+    for (let i=0;i<data.length;i++){
+      if (i===index){
+        array.push(!active[index])
+      } else {
+        array.push(false)
+      }
+    }
+    setActive(array);
+  }
+
 
   return (
     <div className="App" style={{border:"2px solid black"}}>
-      {data.map((Acc)=>{
+      {data.map((Acc,index)=>{
         return (
-          <Accordian content={Acc} />
+          <div className="accordian" style={{border:"2px solid red"}} onClick={()=>changeState(index)}>
+            <div className="header">
+                {Acc.title}
+            </div>
+            {active[index] && <div className="content">
+                {Acc.body}
+            </div>}
+        </div>
         )
       })}
     </div>
